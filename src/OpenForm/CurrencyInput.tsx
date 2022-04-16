1d26de6fd48f14df6925ec1fa5ea37a35c69a43d
Fang-Pen Lin <hello@fangpenlin.com>
2022-04-16 13:57:42 -0700

    Add currencies input

---
+++ src/OpenForm/CurrencyInput.tsx
@@ -0,0 +1,86 @@
+import React, { FunctionComponent } from "react";
+import { GroupBase, Props as SelectProps } from "react-select";
+import CreatableSelect from "react-select/creatable";
+import FormRow from "../Shared/FormRow";
+
+export interface Props {
+  readonly currencies: Array<string>;
+  readonly initialValues?: Array<string>;
+  readonly error?: string;
+  readonly onChange?: (value: string) => void;
+}
+
+interface Option {
+  readonly value: string;
+  readonly label: string;
+}
+
+function CustomSelect<
+  IsMulti extends boolean = true,
+  Group extends GroupBase<Option> = GroupBase<Option>
+>(props: SelectProps<Option, IsMulti, Group>) {
+  return (
+    <CreatableSelect
+      {...props}
+      theme={(theme) => ({ ...theme, borderRadius: 0 })}
+    />
+  );
+}
+
+const CurrencyInput: FunctionComponent<Props> = ({
+  currencies,
+  initialValues,
+  error,
+  onChange,
+}: Props) => {
+  const borderColor = error !== undefined ? "#fd3995" : "#E5E5E5";
+  return (
+    <FormRow title="Currencies" required>
+      <CustomSelect
+        name="currencies"
+        className={error !== undefined ? "is-invalid" : ""}
+        isMulti
+        styles={{
+          option: (provided, state) => ({
+            ...provided,
+            "&:hover": {
+              backgroundColor: "#eeeeee",
+              color: "black",
+            },
+            backgroundColor: state.isSelected ? "#886ab5" : "white",
+          }),
+          control: (provided, state) => ({
+            ...provided,
+            borderRadius: "4px",
+            borderWidth: "1px",
+            borderColor:
+              state.isFocused && error === undefined ? "#886ab5" : borderColor,
+            boxShadow: undefined,
+            "&:hover": undefined,
+            ...(state.isFocused && error !== undefined
+              ? {
+                  boxShadow: "0 0 0 0.2rem rgba(253, 57, 149, 0.25)",
+                }
+              : undefined),
+          }),
+          menu: (provided, state) => {
+            return { ...provided, zIndex: 5 };
+          },
+        }}
+        options={currencies.map((file) => ({ value: file, label: file }))}
+        defaultValue={
+          initialValues !== undefined
+            ? initialValues.map((value) => ({ value, label: value }))
+            : undefined
+        }
+        onChange={(option) => {
+          onChange?.((option as any).value);
+        }}
+      />
+      {error !== undefined ? (
+        <div className="invalid-feedback">{error}</div>
+      ) : null}
+    </FormRow>
+  );
+};
+export default CurrencyInput;
---
+++ storybook/OpenForm/CurrencyInput.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import CurrencyInput from "../../src/OpenForm/CurrencyInput";
+
+export default {
+  title: "OpenForm/CurrencyInput",
+  component: CurrencyInput,
+} as ComponentMeta<typeof CurrencyInput>;
+
+export const Primary: ComponentStory<typeof CurrencyInput> = () => (
+  <CurrencyInput currencies={["USD", "BTC", "TWD"]} />
+);
+
+export const InitialValue: ComponentStory<typeof CurrencyInput> = () => (
+  <CurrencyInput
+    currencies={["USD", "BTC", "TWD"]}
+    initialValues={["BTC", "USD"]}
+  />
+);
+
+export const Error: ComponentStory<typeof CurrencyInput> = () => (
+  <CurrencyInput currencies={["USD", "BTC", "TWD"]} error="Required" />
+);
