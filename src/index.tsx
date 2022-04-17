bae514c86dd610db878bd2b77bc597fdd856bc83
Fang-Pen Lin <hello@fangpenlin.com>
2022-04-16 20:25:18 -0700

    Expose close form

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import TransactionForm, {
   Props as TransactionFormProps,
 } from "./TransactionForm/Form";
 import OpenForm, { Props as OpenFormProps } from "./OpenForm/Form";
+import CloseForm, { Props as CloseFormProps } from "./CloseForm/Form";
 
 // TODO: maybe there's a better way to expose it?
 (window as any).renderTransactionForm = (
@@ -25,6 +26,18 @@ import OpenForm, { Props as OpenFormProps } from "./OpenForm/Form";
   ReactDOM.render(
     <React.StrictMode>
       <OpenForm {...props} />
+    </React.StrictMode>,
+    element
+  );
+};
+
+(window as any).renderCloseForm = (
+  element: HTMLElement,
+  props: CloseFormProps
+) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <CloseForm {...props} />
     </React.StrictMode>,
     element
   );
