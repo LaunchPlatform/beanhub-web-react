b11113968bbe2de0fd01376e4c99f1436f4b9bec
Fang-Pen Lin <hello@fangpenlin.com>
2022-04-17 19:52:04 -0700

    Add renderCommodityForm

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import TransactionForm, {
 } from "./TransactionForm/Form";
 import OpenForm, { Props as OpenFormProps } from "./OpenForm/Form";
 import CloseForm, { Props as CloseFormProps } from "./CloseForm/Form";
+import CommodityForm, {
+  Props as CommodityFormProps,
+} from "./CommodityForm/Form";
 
 // TODO: maybe there's a better way to expose it?
 (window as any).renderTransactionForm = (
@@ -38,6 +41,18 @@ import CloseForm, { Props as CloseFormProps } from "./CloseForm/Form";
   ReactDOM.render(
     <React.StrictMode>
       <CloseForm {...props} />
+    </React.StrictMode>,
+    element
+  );
+};
+
+(window as any).renderCommodityForm = (
+  element: HTMLElement,
+  props: CommodityFormProps
+) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <CommodityForm {...props} />
     </React.StrictMode>,
     element
   );
